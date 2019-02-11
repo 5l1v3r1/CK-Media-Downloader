dbd932a943b49cf7ebd084983827147d9fa42738
JChris246 <chris.401@live.com>
2019-02-11 19:01:49 -0400

    Latest Version (build 26.3)

--- downloaderProject/MainApp.java
+++ downloaderProject/MainApp.java
@@ -71,7 +71,7 @@ public class MainApp extends Application {
     public static ProgressBar progress;
     public static TextArea log;
     public static Actions act;
-    private static final String TITLE = "Video Downloader build 26.2";
+    private static final String TITLE = "Video Downloader build 26.3";
     public static DownloadHistory downloadHistoryList;
     public static StackPane root;
     public static DataCollection habits;
