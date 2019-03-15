3c604857e1269fbd1b0de8857d372974c56f4002
JChris246 <chris.401@live.com>
2019-03-15 19:09:58 -0400

    Change version label and improved size division

--- downloaderProject/MainApp.java
+++ downloaderProject/MainApp.java
@@ -72,7 +72,7 @@ public class MainApp extends Application {
     public static ProgressBar progress;
     public static TextArea log;
     public static Actions act;
-    private static final String TITLE = "Video Downloader build 27.1";
+    private static final String TITLE = "Video Downloader build 27.2";
     public static DownloadHistory downloadHistoryList;
     public static StackPane root;
     public static DataCollection habits;
@@ -316,11 +316,11 @@ public class MainApp extends Application {
         int divide = OS == OsType.Windows ? 1024 : 1000;
         if (size < 0) 
             return "----";
-        else if (size < 1024)
+        else if (size < divide)
             return size + " b";
-        else if((size >= 1024) && (size < 1048576)) //1024b * 1024b = ?kb you do the math
+        else if((size >= divide) && (size < divide * divide)) //1024b * 1024b = ?kb you do the math
             return String.format("%.2f",(double)size / divide) + " kb"; 
-        else if ((size >= 1048576) && (size < 1073741824)) //1024kb * 1024kb = ?mb you do the math
+        else if ((size >= divide * divide) && (size < divide * divide * divide)) //1024kb * 1024kb = ?mb you do the math
             return String.format("%.2f",(double)size / divide / divide) + " mb";
         else return String.format("%.2f",(double)size / divide / divide / divide) + " gb"; //1024mb * 1024mb = ?gb you do the math
     }
