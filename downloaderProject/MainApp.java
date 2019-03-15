331f5463afed4857a1347fc452912499dd65ace8
JChris246 <chris.401@live.com>
2019-03-15 03:17:52 -0400

    Change version label

--- downloaderProject/MainApp.java
+++ downloaderProject/MainApp.java
@@ -72,7 +72,7 @@ public class MainApp extends Application {
     public static ProgressBar progress;
     public static TextArea log;
     public static Actions act;
-    private static final String TITLE = "Video Downloader build 27";
+    private static final String TITLE = "Video Downloader build 27.1";
     public static DownloadHistory downloadHistoryList;
     public static StackPane root;
     public static DataCollection habits;
